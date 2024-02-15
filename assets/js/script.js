08ad5d89f84df9e6963b1d1249234ca6436a71ad
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2024-02-15 15:50:27 -0600

    add bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
 'use strict';
-document.getElementById()
+  window.addEventListener("resize", function() {
+  var w = window.innerWidth;
+  console.log(w)
+  var responsiveDiv = document.querySelectorAll('.responsive');
+
+responsiveDiv.forEach((element) => {
+console.log(element)
+ if(w < 700) {
+        element.classList.remove("desktop");
+        element.classList.add("mobile");
+        console.log("desktop Remove");
+      } else {
+      element.classList.remove("mobile");
+       element.classList.add("desktop");
+       
+        console.log("mobile removed");
+      } 
+
+})
+
+     
+      
+    });
